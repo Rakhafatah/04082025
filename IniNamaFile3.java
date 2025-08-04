dfde0367ba85693363b1d13b9080e2c50d8fc6c7
Rakhafatah <rakha_hariono_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:34:45 +0700

    Add files via upload

---
+++ IniNamaFile3.java
@@ -0,0 +1,47 @@
+public class IniNamaFile2 {
+    public static void main(String[] args) {
+        //main method
+        System.out.println("Fatah");
+        System.out.println("SMK Telkom Malang");
+        System.out.println("SMPN 6 SINGOSARI");
+        System.out.println("nama ayah");
+        System.out.println("nama ibu");
+        System.out.println("Pengusaha");
+        System.out.println("Basket taekwondo");
+
+      //variabel 
+            String nama="Rakha";
+            int usia = 15;
+            System.out.println(nama);
+            System.out.println(usia + " tahun");
+            
+            //Nama teman
+            String namateman1=" Farhan Haidas";
+            String namateman2="Intazar lintang";
+            String namateman3="Azka Rullah";
+            String namateman4="Radittya prima";
+            String namateman5="Fatih Lintang Adzahabi";
+            
+            //Cita-cita teman
+
+            String citacitateman1="pengusaha";
+            String citacitateman2="IT Developer";
+            String citacitateman3="Web Developer";
+            String citacitateman4="Polisi";
+            String citacitateman5="Manager";
+            
+            //Usia Teman
+
+            int usiateman1= 15;
+            int usiateman2= 16;
+            int usiateman3= 15;
+            int usiateman4= 15;
+            int usiateman5= 15;
+
+            System.out.println(nama);
+            System.out.println(nama + usia);
+
+
+
+    }
+}
